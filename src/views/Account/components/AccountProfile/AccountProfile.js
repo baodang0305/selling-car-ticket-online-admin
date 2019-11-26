251f9639a5b9f2cdc3289fa125314e9c54e2a92d
huyhq3 <huyho0202@gmail.com>
2019-11-26 13:30:40 +0700

    template material design + dashboard + chart

---
+++ src/views/Account/components/AccountProfile/AccountProfile.js
@@ -0,0 +1,144 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import moment from 'moment';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Avatar,
+  Typography,
+  Divider,
+  Button,
+  LinearProgress
+} from '@material-ui/core';
+
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  details: {
+    display: 'flex'
+  },
+  avatar: {
+    marginLeft: 'auto',
+    height: 110,
+    width: 100,
+    flexShrink: 0,
+    flexGrow: 0,
+    cursor: 'pointer',
+  },
+  progress: {
+    marginTop: theme.spacing(2)
+  },
+  uploadButton: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const AccountProfile = props => {
+  const { className, ...rest } = props;
+  
+  const [avatar, setAvatar] = useState(null);
+  const [urlAvatar, setUrlAvatar] = useState('');
+  const classes = useStyles();
+
+  const user = {
+    name: 'Shen Zhi',
+    city: 'Los Angeles',
+    country: 'USA',
+    timezone: 'GTM-7',
+    avatar: '/images/avatars/avatar_11.png'
+  };
+
+  const handleChooseFile = e =>{
+    let file = e.target.files[0];
+    setAvatar(file);
+    getBase64(file, imageUrl =>
+      setUrlAvatar(imageUrl)
+    );
+  }
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+        <div className={classes.details}>
+          <div>
+            <Typography
+              gutterBottom
+              variant="h2"
+            >
+              John Doe
+            </Typography>
+            <Typography
+              className={classes.locationText}
+              color="textSecondary"
+              variant="body1"
+            >
+              {user.city}, {user.country}
+            </Typography>
+            <Typography
+              className={classes.dateText}
+              color="textSecondary"
+              variant="body1"
+            >
+              {moment().format('hh:mm A')} ({user.timezone})
+            </Typography>
+          </div>
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="raised-button-file"
+            multiple
+            onChange={(e) => handleChooseFile(e)}
+            style={{ display: 'none' }}
+            type="file"
+          />
+          <label 
+            className={classes.avatar} 
+            htmlFor="raised-button-file"
+          >
+            <Avatar
+              className={classes.avatar} 
+              component="span"
+              src={urlAvatar === '' ? user.avatar : urlAvatar}
+            />
+          </label>
+        </div>
+        <div className={classes.progress}>
+          <Typography variant="body1">Profile Completeness: 70%</Typography>
+          <LinearProgress
+            value={70}
+            variant="determinate"
+          />
+        </div>
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button
+          className={classes.uploadButton}
+          color="primary"
+          component="span"
+          variant="text"
+        >
+          Upload picture
+        </Button>
+        <Button variant="text">Remove picture</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+AccountProfile.propTypes = {
+  className: PropTypes.string
+};
+
+export default AccountProfile;
